949ff5b9663038ab9841e6049bd057b4c1eab8c9
Agus Ramadhani <42666125+buananetpbun@users.noreply.github.com>
2021-03-16 11:51:25 +0700

    Update TCPIP- RESET.bat

--- TCPIP- RESET.bat
+++ TCPIP- RESET.bat
@@ -1,51 +1,42 @@
-@ECHO off
-:: Window Attributes
-TITLE WINDOWS 7-10 TCP/IP Reset and repair
-COLOR 1F
-ECHO.
-ECHO  ÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜ
-ECHO  Ý                                               Ý
-ECHO  Ý Windows 7-10 TCP/IP Reset and repair          Ý
-ECHO  Ý By BuanaNET SECURE, Sept 2019                 Ý
-ECHO  Ý fb.com/buananet.pbun                          Ý
-ECHO  Ý                                               Ý
-ECHO. ßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßß
-ECHO.
-ECHO.
-ECHO ==============================================================
-ECHO # RESET IP ....
-ECHO --------------------------------------------------------------
-netsh int ip reset
-
-ECHO ==============================================================
-ECHO # RESET IP TO DHCP ....
-ECHO --------------------------------------------------------------
-netsh interface ip set address "Local Area Connection" dhcp
-
-ECHO ==============================================================
-ECHO # RESET DNS TO DHCP ....
-ECHO --------------------------------------------------------------
-netsh interface ip set dns "Local Area Connection" dhcp
-
-ECHO ==============================================================
-ECHO # RESET WINSOCK ....
-ECHO --------------------------------------------------------------
-netsh winsock reset
-
-ECHO ==============================================================
-ECHO # CLEAR ARP CACHE ....
-ECHO --------------------------------------------------------------
-netsh interface ip delete arpcache
-
-ECHO ==============================================================
-ECHO # CLEAR DNC CACHE ....
-ECHO --------------------------------------------------------------
-ipconfig /flushdns
-
-ECHO. 
-ECHO --------------------------------------------------------------
-ECHO. "REPAIR DONE...."
-ECHO --------------------------------------------------------------
-
-PAUSE
-EXIT
+ @ECHO off  
+ :: Window Attributes  
+ TITLE WINDOWS 7-10 TCP/IP Reset and repair  
+ COLOR 1F  
+ ECHO.  
+ ECHO ========================================  
+ ECHO = Windows 7-10 TCP/IP Reset and repair 
+ ECHO = By BuanaNET SECURE, Sept 2019
+ ECHO = fb.com/buananet.pangkalanbun  
+ ECHO ========================================
+ ECHO.  
+ ECHO.  
+ ECHO ==============================================================  
+ ECHO # RESET IP ....  
+ ECHO --------------------------------------------------------------  
+ netsh int ip reset  
+ ECHO ==============================================================  
+ ECHO # RESET IP TO DHCP ....  
+ ECHO --------------------------------------------------------------  
+ netsh interface ip set address "Local Area Connection" dhcp  
+ ECHO ==============================================================  
+ ECHO # RESET DNS TO DHCP ....  
+ ECHO --------------------------------------------------------------  
+ netsh interface ip set dns "Local Area Connection" dhcp  
+ ECHO ==============================================================  
+ ECHO # RESET WINSOCK ....  
+ ECHO --------------------------------------------------------------  
+ netsh winsock reset  
+ ECHO ==============================================================  
+ ECHO # CLEAR ARP CACHE ....  
+ ECHO --------------------------------------------------------------  
+ netsh interface ip delete arpcache  
+ ECHO ==============================================================  
+ ECHO # CLEAR DNC CACHE ....  
+ ECHO --------------------------------------------------------------  
+ ipconfig /flushdns  
+ ECHO.   
+ ECHO --------------------------------------------------------------  
+ ECHO. "REPAIR DONE...."  
+ ECHO --------------------------------------------------------------  
+ PAUSE  
+ EXIT  
